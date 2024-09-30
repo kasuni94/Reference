c8b3f7fe492bbc113cd421545725c122d4f53896
kasuni94 <kchamodi@gmail.com>
2024-09-30 12:59:47 +0530

    Add files via upload

---
+++ Tutorial-1-Solution/ConsoleApp1/StudentList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class StudentList
+    {
+        public static List<Student> Students() 
+        {
+            List<Student> slist = new List<Student>();
+
+            Student student1 = new Student();
+            student1.Name = "Robert";
+            student1.University = "Curtin";
+            student1.Id = 101;
+
+            Student student2 = new Student();
+            student2.Name = "Mia";
+            student2.University = "EWU";
+            student2.Id = 102;
+
+            Student student3 = new Student();
+            student3.Name = "Adam";
+            student3.University = "Murdoch";
+            student3.Id = 103;
+
+            Student student4 = new Student();
+            student3.Name = "Duke";
+            student3.University = "UWA";
+            student3.Id = 202;
+
+            Student student5 = new Student();
+            student3.Name = "Mukal";
+            student3.University = "Usyd";
+            student3.Id = 304;
+
+            slist.Add(student1);
+            slist.Add(student2);
+            slist.Add(student3);
+            slist.Add(student4);
+            slist.Add(student5);
+
+            return slist;
+
+
+        }
+    }
+}
---
+++ Tutorial-1-Solution/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ConsoleApp1;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string value = "";
+
+            List<Student> studentlist = StudentList.Students();
+
+            foreach (Student student in studentlist)
+            {
+                value = value + student.ToString()+"\n";
+            }
+
+            TextBox1.Text = value;
+           
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int StudentId = Int32.Parse(StudentID.Text);
+                List<Student> studentlist = StudentList.Students();
+
+                bool found = false;
+                foreach (Student student in studentlist)
+                {
+                    if(student.Id == StudentId)
+                    {
+                        StudentID.Text = student.Id.ToString();
+                        StudentName.Text = student.Name;
+                        StudentUni.Text = student.University.ToString();
+                        found = true;
+                    }
+                }
+                if (!found)
+                {
+                    MessageBox.Show("Could not find, please try again");
+                }
+
+            }
+            catch (Exception exception){
+                MessageBox.Show("Please input valid id");
+                MessageBox.Show(exception.Message);
+            }
+            
+        }
+    }
+}
