67a8ec6df485e205cc2be034fe1061d4410259a6
kasuni94 <kchamodi@gmail.com>
2024-09-30 14:13:10 +0530

    Add files via upload

---
+++ WebApplication1/WebApplication1/WebApplication1/Controllers/AnotherController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnotherController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetResult()
+        {
+            RestClient client = new RestClient("http://localhost:5141");
+            RestRequest request = new RestRequest("/api/Values", Method.Get);
+            RestResponse response = client.Execute(request);
+            String value = JsonConvert.DeserializeObject<String>(response.Content);
+            return Ok(value);
+        }
+    }
+}
