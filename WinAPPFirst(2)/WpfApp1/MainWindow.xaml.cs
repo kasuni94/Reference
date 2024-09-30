e7570a7d3e328503b51c3d5f205e2739847533ae
kasuni94 <kchamodi@gmail.com>
2024-09-30 13:33:03 +0530

    Add files via upload

---
+++ WinAPPFirst(2)/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ConsoleApp1;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string value = "";
+
+            List<Student> studentlist = StudentList.Students();
+
+            foreach (Student student in studentlist)
+            {
+                value = value + student.ToString()+"\n";
+            }
+
+            TextBox1.Text = value;
+           
+        }
+    }
+}
