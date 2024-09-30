a64d7d7d4411aad382ffe5a3a698c1d454b876fe
kasuni94 <kchamodi@gmail.com>
2024-09-30 14:02:44 +0530

    Add files via upload

---
+++ DelegateExample/StudentDatabase/DatabaseServer/StudentServerImplmentation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using StudentDatabase;
+
+
+namespace DatabaseServer
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext= false)]
+
+    internal class StudentServerImplmentation : StudentServerInterface
+    {  
+        int StudentServerInterface.GetNumEntries()
+        {
+            return StudentList.Students().Count;
+        }
+
+        void StudentServerInterface.GetValuesForEntry(int index, out string name, out int id, out string university)
+        {
+            
+            List<Student> slist = StudentList.Students();
+            name = slist[index-1].Name;
+            id = slist[index-1].Id;
+            university = slist[index-1].University;
+        }
+    }
+}
---
+++ DelegateExample/StudentDatabase/StudentBusinessTier/StudentBusinessInterfaceImplimentation.cs
@@ -0,0 +1,61 @@
+﻿using DatabaseServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudentBusinessTier
+{
+    internal class StudentBusinessInterfaceImplimentation : StudentBusinessInterface
+    {
+        private StudentServerInterface foob;
+        public StudentBusinessInterfaceImplimentation()
+        {
+          
+            ChannelFactory<StudentServerInterface> foobFactory;
+            NetTcpBinding tcp = new NetTcpBinding();
+            //Set the URL and create the connection!
+            string URL = "net.tcp://localhost:8100/StudentService";
+            foobFactory = new ChannelFactory<StudentServerInterface>(tcp, URL);
+            foob = foobFactory.CreateChannel();
+           
+
+        }
+
+        public int GetNumEntries()
+        {
+            return foob.GetNumEntries();
+        }
+
+        public void GetValuesForEntry(int index, out string name, out int id, out string university)
+        {
+            foob.GetValuesForEntry(index, out name, out id, out university);
+        }
+
+        public void GetValuesForSearch(string searchText, out string name, out int id, out string university)
+        {
+            name = null;
+            id = 0;
+            university = null;
+            int numEntry = foob.GetNumEntries();
+            for(int i=1; i<=numEntry; i++)
+            {
+                string sName;
+                int sId;
+                string sUni;
+                foob.GetValuesForEntry(i, out sName, out sId, out sUni);
+                if (sName.ToLower().Contains(searchText.ToLower()))
+                {
+                    name = sName;
+                    id = sId;
+                    university = sUni;
+                    break;
+                }
+            }
+            Thread.Sleep(5000); //Forced sleep for two seconds
+        }
+    }
+}
