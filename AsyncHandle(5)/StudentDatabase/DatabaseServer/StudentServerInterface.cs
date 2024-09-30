52e9b34c343d0ea7b71db06dc2d027d71e36b741
kasuni94 <kchamodi@gmail.com>
2024-09-30 13:57:35 +0530

    Add files via upload

---
+++ AsyncHandle(5)/StudentDatabase/DatabaseServer/StudentServerInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseServer
+{
+    [ServiceContract]
+    public interface StudentServerInterface
+    {
+        [OperationContract]
+        int GetNumEntries();
+        [OperationContract]
+        void GetValuesForEntry(int index, out string name, out int id, out string university);
+    }
+}
---
+++ AsyncHandle(5)/StudentDatabase/StudentBusinessTier/StudentBusinessInterface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StudentBusinessTier
+{
+    [ServiceContract]
+    public interface StudentBusinessInterface
+    {
+        [OperationContract]
+        int GetNumEntries();
+        [OperationContract]
+        void GetValuesForEntry(int index, out string name, out int id, out string university);
+
+        [OperationContract]
+        void GetValuesForSearch(string searchText, out string name, out int id, out string university);
+
+    }
+}
